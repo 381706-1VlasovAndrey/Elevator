f9bf9b6c0829dbb5ca7f76c1bd3376fd1e82ad7e
Alice105973 <kukushkinako13081999@gmail.com>
2018-09-23 10:07:53 +0300

    Прописала passenger и service, но если кто-то проверит на работоспособность - будет круто

--- service.cpp
+++ service.cpp
@@ -7,57 +7,36 @@ service::service() {
 void service::button_close()
 {
 	if (!overload) door = false;
-	else cout << "Overload: " << weight - weight_max << "kg" << endl;
 }
 
-bool service::check_weight_max() //ФУНКЦИЯ НЕ НУЖНА!!!
+void service::check_weight_max()
 {
-	// do
-	//			запросить вес, сказать, что это примерно (вес/80) человек
-	// пока (вес не меньше максимального)
-	// дождаться кнопки
-	return true;
+	while (overload) {
+		cout << "Overload: " << weight - weight_max << "kg" << endl << "Enter the weight loss";
+		double loss;
+		cin >> loss;
+		weight -= loss;
+		if (weight <= weight_max) overload = false;
+	}
+	button_close();
 }
 
 void service::in_floor()
 {
-	int a = 0, fl;
-	door = true;
 	double wadd;
-	cout << "1 - someone walked in" << endl << "2 - someone walked out" << endl << "3 - elevator was called" << endl << "4 - close the door" << endl << "5 - choose the floor" << endl;
-	while (a != 4) {
-		cin >> a;
-		cout << endl;
-		switch (a) {
-		case 1: 
-			cout << "Enter additional weight" << endl;
-			cin >> wadd;
-			weight += wadd;
-			break;
-			// проверить перегруз
-		case 2:
-			cout << "Enter lost weight" << endl;
-			cin >> wadd;
-			weight -= wadd;
-			break;
-			// проверить перегруз
-		case 3:
-			cout << "Elevator was called from..?" << endl;
-			cin >> fl;
-			callstack.push_back(fl);
-			//упорядочить
-			break;
-		case 4:
-			button_close();
-			break;
-		case 5:
-			cout << "Enter the floor number" << endl;
-			cin >> fl;
-			callstack.push_back(fl);
-			//упорядочить?
-		default:
-			cout << "Invalid command! Try again" << endl;
-			break;
-		}
-	}
+	check_floor_now();
+	door = true;
+	cout << "Enter the weight loss" << endl;
+	cin >> wadd;
+	weight -= wadd;
+	cout << "Enter the additional weight" << endl;
+	cin >> wadd;
+	weight += wadd;
+	if (weight > weight_max) overload = true;
+	cout << "Enter the floor of destination" << endl;
+	int floor;
+	cin >> floor; 
+	check_weight_max();
+	motion(floor);
+	floor_now = floor;
 }
